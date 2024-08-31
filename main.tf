a083757ec9b74ff02a19872acd858ac0829e7485
shivamadhureddy <164746156+shivamadhureddy@users.noreply.github.com>
2024-08-31 16:42:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "web_server_as" {
-    image_id           = "ami-0195204d5dce06d99"
+    image_id           = "ami-02c21308fed24a8ab"
     instance_type = "t2.micro"
-    key_name = "final"
+    key_name = "docker-pipeline"
     security_groups = [aws_security_group.web_server.id]
 }
    
@@ -10,7 +10,7 @@ resource "aws_launch_configuration" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-0c42873ceef71b275", "subnet-01b9af9baf0cb7e1a"]
+     subnets = ["subnet-09f41513e631f11c1", "subnet-08e08f1b3c5ad755f"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
@@ -29,7 +29,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1b", "us-east-1d"] 
+    availability_zones    = ["us-east-1a", "us-east-1b"] 
     
   }
 
