ec1ee278db2ed3ec7cc14212e6261371e94fadc1
shivamadhureddy <164746156+shivamadhureddy@users.noreply.github.com>
2024-08-31 16:43:03 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafaccit.devops.project.bucket"
+  bucket = "madhu.devops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -26,7 +26,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "mustafaccit.devops.project.bucket"
+bucket = "madhu.devops.project.bucket"
 key = "prod/terraform.tfstate"
 }
 }
