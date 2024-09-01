ab1b1b0b72400d4f67d80c6bc453f0f37187eafa
shivamadhureddy <164746156+shivamadhureddy@users.noreply.github.com>
2024-09-01 08:15:51 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "shivamadhureddy.devops.project.bucket"
+  bucket = "madhureddy.devops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
